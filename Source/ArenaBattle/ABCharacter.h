aac4f9ab5dd7274f8a45071de860ce0d4939d560
Jihyeon4818 <20142349@vision.hoseo.edu>
2020-12-12 18:45:05 +0900

    Character

--- Source/ArenaBattle/ABCharacter.h
+++ Source/ArenaBattle/ABCharacter.h
@@ -2,18 +2,25 @@
 #pragma once
 
 #include "ArenaBattle.h"
-#include "PublicCharacter.h"
+#include "GameFramework/Character.h"
 #include "ChargingBall.h"
 #include "ABCharacter.generated.h"
 
 UCLASS()
-class ARENABATTLE_API AABCharacter : public APublicCharacter
+class ARENABATTLE_API AABCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this character's properties
 	AABCharacter();
+
+	USkeletalMeshComponent* GetSpecificPawnMesh() const;
+
+	FName GetWeaponAttachPoint() const;
+
+	void EquipWeapon(class AABWeapon* Weapon);
+
 
 protected:
 	// Called when the game starts or when spawned
@@ -107,4 +114,26 @@ private:
 	TSubclassOf<class AChargingBall> ChargingBallClass;
 
 	AChargingBall* Ball;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = Inventory)
+		FName WeaponAttachPoint;
+	UPROPERTY()
+		class UABAnimInstance* ABAnim;
+
+
+	TArray<class AABWeapon*> Inventory;
+
+	class AABWeapon* CurrentWeapon;
+
+	void AddWeapon(class AABWeapon* Weapon);
+
+	void SetCurrentWeapon(class AABWeapon* NewWeapon, class AABWeapon* LastWeapon);
+
+
+
+	UPROPERTY(EditDefaultsOnly, Category = Inventory)
+		TArray<TSubclassOf<class AABWeapon>> DefaultInventoryClasses;
+
+	void SpawnDefaultInventory();
 };
--- Source/ArenaBattle/ABWeapon.h
+++ Source/ArenaBattle/ABWeapon.h
@@ -2,7 +2,7 @@
 
 #include "ArenaBattle.h"
 #include "GameFramework/Actor.h"
-#include "PublicCharacter.h"
+#include "ABCharacter.h"
 #include "Engine.h"
 #include "ABWeapon.generated.h"
 
@@ -16,7 +16,7 @@ public:
 	// Sets default values for this actor's properties
 	AABWeapon(const class FObjectInitializer& ObjectInitializer);
 
-	void SetOwningPawn(APublicCharacter* NewOwner);
+	void SetOwningPawn(AABCharacter* NewOwner);
 
 	void AttachMeshToPawn();
 
@@ -27,6 +27,6 @@ public:
 		USkeletalMeshComponent* WeaponMesh;
 
 protected:
-	class APublicCharacter* MyPawn;
+	class AABCharacter* MyPawn;
 
 };
